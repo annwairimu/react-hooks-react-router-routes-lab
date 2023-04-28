e096fcf7297341d556f532bb2777b39953cd193e
Ann Wangui Wairimu <ann99wangui@gmail.com>
2023-04-28 09:13:05 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import Actors from "./Actors";
@@ -7,7 +7,19 @@ import Directors from "./Directors";
 import Movies from "./Movies";
 
 function App() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <Router>
+        <div>
+          <NavBar />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/movies" component={Movies} />
+          <Route exact path="/directors" component={Directors} />
+          <Route exact path="/actors" component={Actors} />
+        </div>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
